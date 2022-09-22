b4c05d1763b0db517ea11acfd79ff1ebbe803648
frank <ipfung@gmail.com>
2022-09-22 11:38:58 +0100

    updated trainer timeslot.

--- src/app/components/trainer-student-list/trainer-student-list.component.ts
+++ src/app/components/trainer-student-list/trainer-student-list.component.ts
@@ -21,6 +21,15 @@ export class TrainerStudentListComponent implements OnInit {
     availableStudents = [];
     formHeader = "Edit Student List";
 
+    // timeslot tab
+    timeslots = [];
+    timeslot: any;
+    weeks = [];
+    tsEditable = false;
+    tsFormDialog = false;
+    tsFormHeader = "Edit Form";
+    tsSubmitted = false;
+
     constructor(private api: ApiService, private router: Router, public lemonade: Lemonade) {
     }
 
@@ -33,6 +42,7 @@ export class TrainerStudentListComponent implements OnInit {
             this.editable = res.editable;
             this.loading = false;
         });
+        this.weeks = this.lemonade.weeks;
     }
 
     edit(trainer) {
@@ -43,6 +53,7 @@ export class TrainerStudentListComponent implements OnInit {
             console.log('/availability-students list=', res);
             this.availableStudents = res.data;
         });
+        this.loadTimeslotData(trainer);
     }
 
     canAmend(trainer) {
@@ -59,10 +70,87 @@ export class TrainerStudentListComponent implements OnInit {
             "teammates": this.trainer.teammates.map((obj) => obj.id),
             "counter": this.trainer.teammates.length
         }).subscribe( res => {
-           console.log('save res=', res);
-           if (res.success == true) {
-               this.hideDialog();
-           }
+            console.log('save res=', res);
+            if (res.success == true) {
+                this.hideDialog();
+            }
+        });
+    }
+
+    /**
+     * Below reference(copied) from timeslot-list.component.ts
+     */
+
+    /**
+     * Below reference(copied) from timeslot-list.component.ts
+     * @param trainer
+     */
+    loadTimeslotData(trainer) {
+        this.api.get('api/trainer-timeslots', {
+            trainer_id: trainer.id
+        }).subscribe( res => {
+            this.timeslots = res.data;
+            this.hideTimeslotDialog();
+            for (let week of this.weeks) {
+                let timeslotData = [];
+                for (let ts of this.timeslots) {
+                    if (ts.day_idx == week.code) {
+                        timeslotData.push(ts);
+                    }
+                }
+                week.data = timeslotData;
+            }
+            this.tsEditable = res.editable;
+        });
+    }
+
+    openNewTimeslot(weekDay) {
+        this.tsFormHeader = "Create Form";
+        this.timeslot = {
+            trainer_id: this.trainer.id,
+            location_id: 1,
+            day_idx: weekDay
+        };
+        this.tsSubmitted = false;
+        this.tsFormDialog = true;
+    }
+
+    /**
+     * reserved for next phase!!
+     * @param weekDay
+     */
+    isCreatingTimeslot(weekDay) {
+        return this.tsFormDialog && this.timeslot.day_idx == weekDay && this.timeslot.id == undefined;
+    }
+
+    isEditingTimeslot(current) {
+        return this.tsFormDialog && this.timeslot && this.timeslot.id && current.id == this.timeslot.id;
+    }
+
+    hideTimeslotDialog() {
+        this.tsFormDialog = false;
+        this.timeslot = {};
+    }
+
+    editTimeslot(timeslot) {
+        this.tsFormHeader = "Edit Form";
+        this.timeslot = {...timeslot};
+        this.tsFormDialog = true;
+    }
+
+    saveTimeslot() {
+        let call;
+        if (this.timeslot.id > 0) {
+            call = this.api.update('api/trainer-timeslots/' + this.timeslot.id, this.timeslot);
+        } else {
+            call = this.api.post('api/trainer-timeslots', this.timeslot);
+        }
+        call.subscribe( res => {
+            console.log('save trainer-timeslots res=', res);
+            if (res.success == true) {
+                this.loadTimeslotData(this.trainer);
+                this.hideTimeslotDialog();
+            }
         });
     }
 }
