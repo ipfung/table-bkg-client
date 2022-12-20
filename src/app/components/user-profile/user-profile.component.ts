16026a47a4bfa9a42815d44ea2c189c502366e3f
frank <ipfung@gmail.com>
2022-12-20 11:41:04 +0000

    updated support Manager to backup appointments via xlsx plugin.

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from "../../service/auth.service";
 import {Lemonade} from "../../service/lemonade.service";
 import {ApiService} from "../../service/api.service";
 import {MessageService} from "primeng/api";
+import * as FileSaver from 'file-saver';
 
 @Component({
     selector: 'app-user-profile',
@@ -28,6 +29,9 @@ export class UserProfileComponent implements OnInit {
     }
 
     async ngOnInit() {
+        this.obj = {
+            role_color_name: ''
+        };
         this.avatar = await this.authService.avatar();
         this.email = await this.authService.email();
         this.userName = await this.authService.userName();
@@ -68,5 +72,47 @@ export class UserProfileComponent implements OnInit {
                 this.lemonade.error(this.messageService, res);
             }
         });
+    }
+
+    exportAppointmentData(list) {
+        return list.map(row => ({
+            customer_name: row.customer_name,
+            price: row.price,
+            start_time: row.start_time,
+            end_time: row.end_time,
+            room: row.name,
+            check_in: row.checkin,
+            check_out: row.checkout,
+            trainer_name: row.trainer_id != row.customer_id ? row.user_name : '',
+            package_name: row.package_name,
+            appointment_status: row.status,
+            payment_status: row.payment_status,
+            created_at: row.created_at,
+            updated_at: row.updated_at
+        }));
+    }
+
+    exportExcel() {
+        import('xlsx').then((xlsx) => {
+            this.api.get('api/booking', {
+                from_date: '2022-01-01',
+                to_date: '2099-12-31'
+            }).subscribe(res => {
+                const data = this.exportAppointmentData(res.data);
+                const worksheet = xlsx.utils.json_to_sheet(data);
+                const workbook = { Sheets: { data: worksheet }, SheetNames: ['Appointment'] };
+                const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+                this.saveAsExcelFile(excelBuffer, 'appointments');
+            });
+        });
+    }
+
+    saveAsExcelFile(buffer: any, fileName: string): void {
+        let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        let EXCEL_EXTENSION = '.xlsx';
+        const data: Blob = new Blob([buffer], {
+            type: EXCEL_TYPE
+        });
+        FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
     }
 }
