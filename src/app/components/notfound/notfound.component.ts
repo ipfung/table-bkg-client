d5477ebd2f0c174ca1d57734c6904d2aee81cf9e
frank <ipfung@gmail.com>
2023-04-19 07:07:37 +0100

    updated not found page.

--- src/app/components/notfound/notfound.component.ts
+++ src/app/components/notfound/notfound.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import {ConfigService} from "../../service/app.config.service";
+import {AppConfig} from "../../api/appconfig";
 
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.component.html',
 })
-export class NotfoundComponent {}
+export class NotfoundComponent {
+    config: AppConfig;
+
+    constructor(public configService: ConfigService) {
+    }
+
+    ngOnInit() {
+        this.config = this.configService.config;
+    }
+}
