3981a878f745ab81aef5eed5f4dda09e53595fe6
frank <ipfung@gmail.com>
2023-08-13 10:34:53 +0100

    updated supports capacitor deeplink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 import {AuthService} from "./service/auth.service";
 import {ConfigService} from "./service/app.config.service";
 import {TranslateService} from "@ngx-translate/core";
+
+import {App, URLOpenListenerEvent} from "@capacitor/app";
+import {Router} from "@angular/router";
+
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
@@ -11,7 +16,9 @@ export class AppComponent {
 
     menuMode = 'static';
 
-    constructor(private primengConfig: PrimeNGConfig, private configService: ConfigService, private authService: AuthService, private translate: TranslateService) { }
+    constructor(private primengConfig: PrimeNGConfig, private router: Router, private zone: NgZone, private configService: ConfigService, private authService: AuthService, private translate: TranslateService) {
+        this.initializeApp();
+    }
 
     ngOnInit() {
         // change theme based on env.
@@ -24,6 +31,21 @@ export class AppComponent {
         this.translate.setDefaultLang('zh');
         this.translate.get('primeng').subscribe(res => this.primengConfig.setTranslation(res));
         // console.log('lang===', this.translate.getDefaultLang());
+    }
+
+    initializeApp() {
+        App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+            this.zone.run(() => {
+                // Example url: https://beerswift.app/tabs/tab2
+                // slug = /tabs/tab2
+                const slug = event.url.split(".com").pop();
+                if (slug) {
+                    this.router.navigateByUrl(slug);
+                }
+                // If no match, do nothing - let regular routing
+                // logic take over
+            });
+        });
     }
 
     isAutorized(): boolean {
